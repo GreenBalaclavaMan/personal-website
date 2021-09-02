47e029490f656ea611add53e9f61b0e86dc656e2
GreenBalaclavaMan <greenBalaclavaMan@protonmail.com>
2021-09-02 18:09:44 -0400

    yeh

---
+++ footer.php
@@ -0,0 +1,15 @@
+          <!--Footer-->
+          <div class="div-footer">
+               <p>
+                    This project is open source, <a href="https://github.com/GreenBalaclavaMan/WebOTP">check out WebOTP on github</a>.
+               </p>
+               <p>
+                    &copy;2021 Nicholas Rinella
+               </p>
+          </div>
+
+          </div>
+          <!-- end of main page content -->
+
+     </body>
+</html>
